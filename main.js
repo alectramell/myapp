02c4b4705469c7d3a0e115695fc856822c3bfb7a
alectramell <alectramell@gmail.com>
2017-04-25 17:03:37 -0700

    myapp

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var date = new Date();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+
+function ZOOM() {document.body.style.zoom = "100%"};
+function TIME() {var time = (alert(hours + ":" + minutes))};
+function NAME() {var name = (alert("Hyrule"))};
+function HOME() {var name = (alert("Home"))};
+function CASTLE() {var name = (alert("Hyrule Castle"))};
+
+
+
